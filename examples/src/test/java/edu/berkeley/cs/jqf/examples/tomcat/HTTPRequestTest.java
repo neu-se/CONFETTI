a8c602b9bae8585cadd84c669dc01f9bc8576316
James Kukucka <jkukucka@gmu.edu>
2019-03-19 17:16:12 -0400

    Adding code to HttpRequestTest

--- examples/src/test/java/edu/berkeley/cs/jqf/examples/tomcat/HTTPRequestTest.java
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/tomcat/HTTPRequestTest.java
@@ -5,19 +5,34 @@ import edu.berkeley.cs.jqf.examples.common.Dictionary;
 import edu.berkeley.cs.jqf.examples.http.HTTPRequestGenerator;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
 import edu.berkeley.cs.jqf.fuzz.JQF;
+import org.apache.coyote.http11.HeaderParser;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.junit.Assume;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
 
 
 @RunWith(JQF.class)
 public class HTTPRequestTest {
 
     @Fuzz
-    public void parseHTTPRequest(@From(HTTPRequestGenerator.class) HttpUriRequest req) {
+    public void parseHTTPRequest(byte[] input) {
+        Assume.assumeTrue(input.length < 200);
+        HeaderParser parser = new HeaderParser(input);
         try {
-            System.out.println(req.toString());
+            parser.parseHeaders();
+        } catch (IllegalArgumentException e) {
+            Assume.assumeNoException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Fuzz
+    public void parseHTTPRequestWithGenerator(@From(HTTPRequestGenerator.class) HttpUriRequest req) {
+        try {
+            parseHTTPRequest(req.toString().getBytes());
         } catch (IllegalArgumentException e) {
             Assume.assumeNoException(e);
         }
