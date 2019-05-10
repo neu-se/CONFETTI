25fc6316ec77bf04efe437db392d3347002e5125
Luis Pina <luis@luispina.me>
2019-05-10 17:25:42 -0400

    Improved classpath script for closure

--- closure/classpath.sh
+++ closure/classpath.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo `find /home/lganchin/repos/jqf/closure/deps -maxdepth 1 -type f` | sed 's/ /:/g'
+# Figure out script absolute path
+pushd `dirname $0` > /dev/null
+SCRIPT_DIR=`pwd`
+popd > /dev/null
+
+# The root dir is one up
+ROOT_DIR=`dirname $SCRIPT_DIR`
+
+# Create classpath
+cp=""
+
+for jar in $SCRIPT_DIR/deps/*.jar; do
+  cp="$cp:$jar"
+done
+
+echo $cp
